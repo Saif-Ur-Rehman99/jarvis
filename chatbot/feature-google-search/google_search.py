4dfb497f1a75b84296d8b1f1cb85f6e5196af5e2
Saif-Ur-Rehman99 <saif.khan@xloopdigital.com>
2024-03-14 12:58:59 +0500

    google search tool is working fine

--- chatbot/feature-google-search/google_search.py
+++ chatbot/feature-google-search/google_search.py
@@ -1,29 +1,14 @@
+import os
 import sys
 sys.path.append('/home/saif/Documents/jarvis/chatbot')
 from chatbot import chat_openai
-from langchain.agents import Tool, load_tools, create_openai_functions_agent
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
-from langchain_core.messages import SystemMessage, AIMessage, HumanMessage
+from langchain.agents import Tool, initialize_agent
 from langchain_community.utilities.serpapi import SerpAPIWrapper
-from langchain_community.utilities.you import YouSearchAPIWrapper
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 llm = chat_openai('gpt-3.5-turbo')
-
-
-# Create the ChatPromptTemplate
-prompt = ChatPromptTemplate.from_messages(
-    [
-        SystemMessage(
-            content='you are a helper'
-        ),
-        MessagesPlaceholder(variable_name="agent_scratchpad"),
-    ]
-)
-
 
 # Google Search
 search = SerpAPIWrapper(
@@ -35,25 +20,20 @@ search = SerpAPIWrapper(
     serpapi_api_key=os.getenv('SERPAPI_API_KEY')
 )
 
-print(type(search))
-
-tools = load_tools({"serpapi": search})
-
-agent = create_openai_functions_agent(
-    llm,
-    tools,
-    prompt,
+search_tool = Tool(
+    name = "Web Search",
+    func=search.run,
+    description="A useful tool for searching the Internet to find information on world events, issues, etc. Worth using for general topics. Use precise questions."
 )
 
-agent.invoke('who is the current president of Pakistan')
 
+agent = initialize_agent(
+    agent="zero-shot-react-description",
+    tools=[search_tool],
+    llm=llm,
+    verbose=True,
+    max_iterations=3
+)
 
-
-
-# repl_tool = Tool(
-#     name="python_repl",
-#     description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
-#     func=search.run('?'),
-# )
-
-# print(type(repl_tool))
+response = agent('who is the current president of Pakistan')
+print(response)
